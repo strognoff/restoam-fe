110a4a3a2a317f695eea2e63ac4800b4cfa92d85
Arroz com carne <jcoliveira@gmail.com>
2025-06-12 15:20:14 +0100

    Added GoTo Menubar
    Menu bar was added to Navbar and dependencies in index.html

--- restoam-fe/src/components/Navbar.js
+++ restoam-fe/src/components/Navbar.js
@@ -4,10 +4,47 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        <img src="/images/gear.png" width="30" height="30" className="d-inline-block align-top" alt="" />
-        RestoAM: Asset
-      </a>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img src="/images/gear.png" width="30" height="30" className="d-inline-block align-top" alt="" />
+          RestoAM: Asset
+        </a>
+      </nav>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Go To
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Dashboard</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Asset</a>
+              <a className="dropdown-item" href="#">Location</a>
+              <a className="dropdown-item" href="#">WorkOrder</a>
+            </div>
+          </li>
+        </ul>
+        </div>
     </nav>
   );
 }
