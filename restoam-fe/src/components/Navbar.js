1045aa698aadfe5047605af1fdc1b30ec0799caa
Arroz com carne <jcoliveira@gmail.com>
2025-06-17 10:34:06 +0100

    Updated the title of RestoAM
    Added the meaning of AMS

--- restoam-fe/src/components/Navbar.js
+++ restoam-fe/src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <nav class="navbar navbar-light bg-light">
       <a class="navbar-brand" href="#">
         <img src="images/gear.png" width="30" height="30" class="d-inline-block align-top" alt=""></img>
-        RestoAM: Asset
+        RestoAM - Asset Management System
       </a>
     </nav>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
